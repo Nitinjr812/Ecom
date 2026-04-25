b28edec6360f040c27a7f3d64c2ac5cda0cb4646
Nitinjr812 <nitinjr812@gmail.com>
2026-04-25 14:09:53 +0530

    fixed dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
-<<<<<<< HEAD
-=======
 import Auth from "./pages/Auth"
->>>>>>> nitin
 
 function App() {
 
@@ -12,10 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" Component={Navbar} />
-<<<<<<< HEAD
-=======
           <Route path="/Auth" Component={Auth} />
->>>>>>> nitin
         </Routes>
       </BrowserRouter>
     </>
